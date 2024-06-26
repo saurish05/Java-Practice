1942ff306134e406ceba05661c2f0a15a34d1216
saurish05 <saurish.krovvidi@gmail.com>
2024-06-26 10:49:36 -0500

    Delete Java practice directory

--- Java practice/FirstJavaCode.java
+++
@@ -1,8 +0,0 @@
-class FirstJavaCode{
-public static void main(String hi[]){
-	System.out.println("Hello world");
-	System.out.print("saurish Krovvidi");
-	System.out.println("Hello world");
-	System.out.printf("Hello world");
-	}
-}
