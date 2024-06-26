cb274523f94e7aab636b7525588598241825a1f6
saurish05 <saurish.krovvidi@gmail.com>
2024-06-26 10:44:12 -0500

    practiced on june 26

---
+++ Java practice/AOTest.java
@@ -0,0 +1,55 @@
+/*
+class AOTest {
+	public static void main(String []args){
+
+		 long val1 = 500;
+		 int val2 = 20;
+		 long varAns = (val1 + val2);
+
+		System.out.println(varAns);
+	}
+}
+*/
+
+
+
+
+/*
+class AOTest {
+	public static void main(String []args){
+
+		 char val1 = 'a';
+		 int val2 = 20;
+		 long varAns = (val1 + val2);
+
+		System.out.println(varAns);
+	}
+}
+*/
+
+
+
+/*
+class AOTest {
+	public static void main(String []args){
+
+		 long val1 = 500;
+		 int val2 = 20;
+		 int varAns = (int)(val1 + val2);
+
+		System.out.println(varAns);
+	}
+}
+
+*/
+
+class AOTest {
+	public static void main(String []args){
+
+		 float val1 = 556.345f;
+		 int val2 = 20;
+		 double varAns = (val1 + val2);
+
+		System.out.println(varAns);
+	}
+}
